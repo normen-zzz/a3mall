e10399b09a36748c9db6364985e080e5ac9408c2
normanardian <71215112+normen-zzz@users.noreply.github.com>
2022-03-16 12:25:19 +0700

    Update footer.php

--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -1,8 +1,5 @@
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2018 <div class="bullet"></div>
-    </div>
-    <div class="footer-right">
-        2.3.0
+        Copyright &copy; 2022 <div class="bullet"></div>
     </div>
 </footer>
